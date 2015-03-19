452a272715d8ba6ae2a427820a1aedb3e2f9ff0d
BlueTheFrog <ochoad64@gmail.com>
2015-03-19 08:43:36 -0400

    edited platformer level

---
+++ Assets/Scripts/Platformer/RotatePlatform.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotatePlatform : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    transform.eulerAngles = new Vector3 (0f, 0f, tranform.eulerAngles.z);
+	
+	}
+}
